b59c4b8b084e5ae241340538e8f60f5f1434ade6
Harish Krishnan <justcodedaily@gmail.com>
2026-04-05 21:03:33 +0200

    Add Anlytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { EggHuntDashboard } from "./components/EggsDashboard";
+import { Analytics } from "@vercel/analytics/next"
 
 const Index = () => {
-  return <EggHuntDashboard />;
+  return (
+    <>
+        <Analytics/>
+        <EggHuntDashboard/>
+    </>
+  ) 
 };
 
 export default Index;
